36d8c38e54a0ae464516a5993ebc0bae51cf56a1
PiyushDatta <piyushdattaca@gmail.com>
2021-12-26 17:35:35 -0500

    AC for https://codeforces.com/contest/791/problem/A

---
+++ junior_training_A/791_a.cc
@@ -0,0 +1,66 @@
+// https://codeforces.com/contest/791/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+// uncomment this line when submitting
+// #define _MY_TEST_FILES true
+// uncomment if the submission DOES NOT require you take multiple test cases
+// #define _MULTIPLE_TEST_CASES true
+// uncomment if the submission DOES NOT require you to print the case number
+// #define _PRINT_CASE_NUMBER true
+
+/*
+=================================
+THIS IS WHERE YOUR ALGO GOES
+=================================
+*/
+void solve() {
+  int a = 0;
+  int b = 0;
+  cin >> a;
+  cin >> b;
+  int res = 0;
+  while (a <= b) {
+    ++res;
+    a *= 3;
+    b *= 2;
+  }
+
+  cout << res << endl;
+}
+/*
+=================================
+ABOVE IS WHERE YOUR ALGO GOES
+=================================
+*/
+
+int main(int argc, char const *argv[]) {
+  // optional performance optimizations
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+#ifdef _MY_TEST_FILES
+  // for getting input from input.txt file
+  freopen("input.txt", "r", stdin);
+  // printing the output to output.txt file
+  freopen("my_ans.txt", "w", stdout);
+#endif
+
+#ifdef _MULTIPLE_TEST_CASES
+  // keep this, testcases
+  int t = 0;
+  cin >> t;
+
+  // now we can run our algo
+  for (int i = 1; i <= t; ++i) {
+#ifdef _PRINT_CASE_NUMBER
+    cout << "Case #" << i << ": ";
+#endif
+    solve();
+  }
+#else
+  solve();
+#endif
+
+  return 0;
+}
