ca0a8c1b0cb42d80d7c707580a76670a5d7ab18e
PiyushDatta <piyushdattaca@gmail.com>
2022-10-16 15:11:05 -0400

    https://codeforces.com/problemset/problem/1598/A

---
+++ A_Computer_Game.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// comment out if the submission DOES NOT require you take multiple test cases
+#define _MULTIPLE_TEST_CASES true
+// comment out if the submission DOES NOT require you to print the case number
+// #define _PRINT_CASE_NUMBER true
+// comment out when submitting submission
+// #define _USE_INPUT_OUTPUT_FILES true
+
+/*
+=================================
+THIS IS WHERE YOUR ALGO GOES
+=================================
+*/
+void solve() {
+  int n = 0;
+  cin >> n;
+  vector<char> r1(n, '0');
+  vector<char> r2(n, '0');
+
+  for (int i = 0; i < n; ++i) {
+    cin >> r1[i];
+  }
+
+  for (int i = 0; i < n; ++i) {
+    cin >> r2[i];
+  }
+
+  for (int i = 0; i < n; ++i) {
+    if (r1[i] == '1' && r2[i] == '1') {
+      cout << "NO" << endl;
+      return;
+    }
+  }
+  cout << "YES" << endl;
+}
+/*
+=================================
+ABOVE IS WHERE YOUR ALGO GOES
+=================================
+*/
+
+int main(int argc, char const *argv[]) {
+  // optional performance optimizations
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+#ifdef _USE_INPUT_OUTPUT_FILES
+  // for getting input from input.txt file
+  freopen("old_way_to_run/input.txt", "r", stdin);
+  // printing the output to output.txt file
+  freopen("old_way_to_run/my_ans.txt", "w", stdout);
+#endif
+
+#ifdef _MULTIPLE_TEST_CASES
+  // keep this, testcases
+  int t = 0;
+  cin >> t;
+
+  // now we can run our algo
+  for (int i = 1; i <= t; ++i) {
+#ifdef _PRINT_CASE_NUMBER
+    cout << "Case #" << i << ": ";
+#endif
+    solve();
+  }
+#else
+  solve();
+#endif
+
+  return 0;
+}
